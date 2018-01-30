2fcc57a59799171a9885fbe029b1bde76c9eb125
addaskogberg <adda.skogberg@gmail.com>
2018-01-30 14:43:06 +0100

    skrapa länkar

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const JSDOM = require('jsdom').JSDOM
+
 const request = require('request')
+const JSDOM = require('jsdom').JSDOM
 
-console.log('testing start')
-
-console.log(process.argv)
+// console.log(process.argv)
 let args = process.argv.slice(2)
 
 if (args.length === 0) {
@@ -15,10 +14,41 @@ console.log(startURL)
 
 async function start (url) {
   let data = await asyncRequest(url)
+  // console.log(data)
   return data
 }
 
 let data = start(startURL)
+data.then(function (html) {
+  let links = parseHTML(html)
+  links = links.map(alinks => alinks.href)
+  if (links.length < 1) {
+    throw new Error('could not find any links')
+  }
+  console.log(links)
+}).catch(function (error) {
+  console.log(error.message)
+})
+
+function parseHTML (html) {
+  const dom = new JSDOM(html)
+  return Array.from(dom.window.document.querySelectorAll('a'))
+}
+// console.log(data)
+
+function asyncRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error)
+      }
+      resolve(body)
+    })
+  })
+}
+
+/*
+
 data.then(function (html) {
   let links = parseHTML(html)
   links = links.map(alinks => alinks.href)
@@ -45,3 +75,4 @@ function asyncRequest (url) {
     })
   })
 }
+ */
