2fe30d1326d288243d355a63825519ae9d4064e6
addaskogberg <adda.skogberg@gmail.com>
2018-01-30 09:41:20 +0100

    försöker förstå går sådär

--- app.js
+++ app.js
@@ -1,2 +1,47 @@
+const JSDOM = require('jsdom').JSDOM
+const request = require('request')
 
 console.log('testing start')
+
+console.log(process.argv)
+let args = process.argv.slice(2)
+
+if (args.length === 0) {
+  console.log('ERROR: No arguments provided e.g "npm start <url>"')
+  process.exit(0)
+}
+const startURL = args[0]
+console.log(startURL)
+
+async function start (url) {
+  let data = await asyncRequest(url)
+  return data
+}
+
+let data = start(startURL)
+data.then(function (html) {
+  let links = parseHTML(html)
+  links = links.map(alinks => alinks.href)
+  if (links.length < 1) {
+    throw new Error('could not find any links')
+  }
+}).catch(function (error) {
+  console.log(error.message)
+})
+
+function parseHTML (html) {
+  const dom = new JSDOM(html)
+  // return Array.from(dom.window.document.querySelectorAll('a'))
+  console.log('test' + Array.from(dom.window.document.querySelectorAll('a')))
+}
+
+function asyncRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (error) {
+        throw (error)
+      }
+      return body
+    })
+  })
+}
