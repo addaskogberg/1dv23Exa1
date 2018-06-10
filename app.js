deb46f2cee5a94d48c7f8ee9a81bbd6c1b26e52e
addaskogberg <adda.skogberg@gmail.com>
2018-06-10 16:02:34 +0200

    calendar

--- app.js
+++ app.js
@@ -4,28 +4,18 @@
  */
 
 'use strict'
-var cherrio = require('cheerio')
-
-var cinema = require('./cinema')
+// var cinema = require('./cinema')
 var calendar = require('./calendar')
 
 const request = require('request-promise')
 
 var url = process.argv[2] || 'http://vhost3.lnu.se:20080/calendar/'
-var calendarURL
-var cinemaURL
-var restaurantURL
+let calendarURL
 
-/**
- * has the url http
- *
- * @param {any} url
- * @returns
- */
-function checkUrl (url) {
+function validate (url) {
   return new Promise(function (resolve, reject) {
     if (url.substr(0, 4) !== 'http') {
-      reject(error)
+     // reject(error)
     }
 
     request(url).then(function () {
@@ -36,14 +26,16 @@ function checkUrl (url) {
   })
 }
 
-checkUrl.url(url).then(function () {
-  // Get links to calendar, cinema, and restaurant from the front page
+validate(url).then(function () {
+    // Get links to calendar, cinema, and restaurant from the front page
   return calendar.getLinks(url)
 }).then(function (links) {
-  // Store links to global variables
+    // Store links to global variables
   calendarURL = links[0]
-  cinemaURL = links[1]
-  restaurantURL = links[2]
 
-  console.log(cinema)
+    // Get links to the calendar page of each person
+  return calendar.getLinks(calendarURL)
+}).then(function (links) {
+    // Get all friends and their what their status is on each day
+  return calendar.calendars(links)
 })
