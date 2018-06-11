421aa2673e3d43d4cbdb7fc2455411e90a489f82
addaskogberg <adda.skogberg@gmail.com>
2018-06-11 13:13:42 +0200

    get the available bookings from restaurant

--- restaurant.js
+++ restaurant.js
@@ -4,7 +4,23 @@
  */
 
 'use strict'
+
+const cheerio = require('cheerio')
+
 // find the booking alternatives for the restaurant. look at what time the films are and find a booking
+function restaurantList (html) { // nr1 findBookingAlternatives
+  let restaurantBookings = []
+  let control = cheerio.load(html)
+  let findPage = control('input[name=group1]')
+
+  control(findPage).each(function (i, element) {
+    restaurantBookings.push(parseBooking(control(element).val()))
+  })
+  console.log(html + 'är i restaurantList')
+  return restaurantBookings
+}
+
+console.log(restaurantList)
 
 // analyze the  booking alternatives
 // booking alternatives
